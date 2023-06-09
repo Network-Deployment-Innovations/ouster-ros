bd6ff694101365d5fd74645b5f1c258823aeb0df
John Lancaster <126011192+crowncastlejsl@users.noreply.github.com>
2023-06-09 11:24:10 -0500

    changed logging to debug level for os_sensor and os_cloud nodes

--- ouster-ros/launch/sensor.independent.launch.py
+++ ouster-ros/launch/sensor.independent.launch.py
@@ -46,6 +46,7 @@ def generate_launch_description():
         namespace=ouster_ns,
         parameters=[params_file],
         output='screen',
+        arguments=['--ros-args', '--log-level', 'debug']
     )
 
     sensor_configure_event = EmitEvent(
@@ -90,6 +91,7 @@ def generate_launch_description():
         namespace=ouster_ns,
         parameters=[params_file],
         output='screen',
+        arguments=['--ros-args', '--log-level', 'debug']
     )
 
     os_image = Node(
